f3a17563b57bce2036c588e76e2d120979a7962c
William Goulart <williamgoulart1@hotmail.com>
2023-02-06 22:43:33 -0400

    feat(Calculator): Adding the news methods to calculator, divided, minus, and multiply

--- modulo_1/project-01/src/lib/calculator.js
+++ modulo_1/project-01/src/lib/calculator.js
@@ -3,7 +3,37 @@ module.exports.sum = (firstNumber, secondNumber) => {
   const numberTwo = parseInt(secondNumber);
 
   if (Number.isNaN(numberOne) || Number.isNaN(numberTwo)) {
-    throw new Error('Please check the input value')
+    throw new Error('Please check the input value');
   }
-    return parseInt(firstNumber) + parseInt(secondNumber);
+  return parseInt(firstNumber) + parseInt(secondNumber);
+};
+
+module.exports.minus = (firstNumber, secondNumber) => {
+  const numberOne = parseInt(firstNumber);
+  const numberTwo = parseInt(secondNumber);
+
+  if (Number.isNaN(numberOne) || Number.isNaN(numberTwo)) {
+    throw new Error('Please check the input value');
+  }
+  return parseInt(firstNumber) - parseInt(secondNumber);
+};
+
+module.exports.divide = (firstNumber, secondNumber) => {
+  const numberOne = parseInt(firstNumber);
+  const numberTwo = parseInt(secondNumber);
+
+  if (Number.isNaN(numberOne) || Number.isNaN(numberTwo)) {
+    throw new Error('Please check the input value');
+  }
+  return parseInt(firstNumber) / parseInt(secondNumber);
+};
+
+module.exports.multiply = (firstNumber, secondNumber) => {
+  const numberOne = parseInt(firstNumber);
+  const numberTwo = parseInt(secondNumber);
+
+  if (Number.isNaN(numberOne) || Number.isNaN(numberTwo)) {
+    throw new Error('Please check the input value');
+  }
+  return parseInt(firstNumber) * parseInt(secondNumber);
 };
--- modulo_1/project-01/src/lib/calculator.spec.js
+++ modulo_1/project-01/src/lib/calculator.spec.js
@@ -1,27 +1,57 @@
-const { sum } = require('./calculator');
+const { sum, minus, divide, multiply} = require('./calculator');
 
-it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum(2, 2)).toBe(4);
+describe('Calculator SUM', () => {
+  it('should sum 2 and 2 and the result must be 4', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+    expect(sum('2', '2')).toBe(4);
+  });
+
+  it('should throw an error if what is provided to the method cannot be summed', () => {
+    expect(() => {
+      sum('', '2');
+    }).toThrowError();
+
+    expect(() => {
+      sum([2, 2]);
+    }).toThrowError();
+
+    expect(() => {
+      sum({});
+    }).toThrowError();
+
+    expect(() => {
+      sum();
+    }).toThrowError();
+  });
 });
 
-it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
-  expect(sum('2', '2')).toBe(4);
-});
 
-it('should throw an error if what is provided to the method cannot be summed', () => {
-  expect(() => {
-    sum('', '2');
-  }).toThrowError();
+describe('Calculator Minus', () => {
+  it('should minus 2 and 2 and te result must be 8', () => {
+    expect(minus(10, 2)).toBe(8)
+  })
+  it('should minus 2 and 2 even if one of them is a string and the result must be 4', () => {
+    expect(minus('2', '2')).toBe(0);
+  });
+})
 
-  expect(() => {
-    sum([2, 2]);
-  }).toThrowError();
+describe('Calculator Divide', () => {
+  it('should divide 2 and 2 and te result must be 5', () => {
+    expect(divide(10, 2)).toBe(5)
+  })
+  it('should divide 2 and 2 even if one of them is a string and the result must be 0', () => {
+    expect(divide('2', '2')).toBe(1);
+  });
+})
 
-  expect(() => {
-    sum({});
-  }).toThrowError();
-  
-  expect(() => {
-    sum();
-  }).toThrowError();
-});
+describe('Calculator multiply', () => {
+  it('should multiply 2 and 2 and te result must be 20', () => {
+    expect(multiply(10, 2)).toBe(20)
+  })
+  it('should multiply 2 and 2 even if one of them is a string and the result must be 4', () => {
+    expect(multiply('2', '2')).toBe(4);
+  });
+})
